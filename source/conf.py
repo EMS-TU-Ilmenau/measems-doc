adf512fa9ffb580d053decc6e75ddd8b3203efa8
Sebastian Semper <sebastian.semper@tu-ilmenau.de>
2025-03-14 14:14:25 +0100

    new theme

--- source/conf.py
+++ source/conf.py
@@ -1,28 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+project = "EMS Measurements Documentation"
+copyright = "2025, EMS Group, TU Ilmenau and Fraunhofer IIS, Ilmenau"
+author = "EMS Group, TU Ilmenau and Fraunhofer IIS, Ilmenau"
+release = "1.0"
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+extensions = ["sphinx_rtd_theme"]
 
-project = 'EMS Measurements Documentation'
-copyright = '2025, EMS Group, TU Ilmenau and Fraunhofer IIS, Ilmenau'
-author = 'EMS Group, TU Ilmenau and Fraunhofer IIS, Ilmenau'
-release = '1.0'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
